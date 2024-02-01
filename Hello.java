fb1903dd1af5cd249ab26307425f3f091c1ec04f
tal7997 <125656345+tal7997@users.noreply.github.com>
2024-02-01 20:27:39 +0200

    update print message

--- Hello.java
+++ Hello.java
@@ -2,7 +2,7 @@ public class Hello{
     public static void main(String[] args){
         for(int i=1;i<=5;i++)
         {
-            System.out.println("Hello World ... "+i);
+            System.out.println("Hello it's me ... "+i);
             System.out.println("------------------");
         }
     }
