a0f4da67f321c597c39ce248791d438fff9e39f7
tal7997 <125656345+tal7997@users.noreply.github.com>
2024-02-01 20:31:11 +0200

    change print name

--- yourName.java
+++ yourName.java
@@ -2,7 +2,7 @@ public class yourName{
     public static void main(String[] args){
         for(int i=1;i<=5;i++)
         {
-            System.out.println("Tal Zechariya");
+            System.out.println("El salvador");
             System.out.println("------------------");
         }
     }
